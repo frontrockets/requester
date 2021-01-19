8f76731ef249736a7292be88c031111c0cea12d2
Andrew Kuzmichev <kuzmichev.2012@mail.ru>
2021-01-19 18:04:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+_This is part of @datarockets infrastructure_
+
+## Install
+
+```
+npm install @frontrockets/requester
+```
+
+## Usage
+
+```jsx
+import createAbstractAction from "@frontrockets/requester";
+
+// Requester uses Axios under the hood, expect relevant to Axios config
+
+const api = new Requester({
+  baseURL: process.env.REACT_APP_API,
+  withCredentials: true,
+  injectHeaders: () => ({
+    Authorization: `Bearer ${SessionToken.get()}`,
+  }),
+  transformResponse: (response) => response.data,
+});
+
+api.get("/some-url");
+api.post("/some-url");
+api.patch("/some-url");
+api.delete("/some-url");
+```
---
+++ src/index.test.js
@@ -0,0 +1,165 @@
+import axios from "axios";
+
+import Requester from "./";
+
+const mockAxiosInstance = {
+  request: jest.fn(() => null),
+};
+
+beforeEach(() => {
+  axios.create = jest.fn(() => mockAxiosInstance);
+  mockAxiosInstance.request.mockReset();
+});
+
+/* eslint-disable no-new */
+
+it("passes the config to axios", () => {
+  const config = { test: "$$test" };
+
+  new Requester(config);
+
+  expect(axios.create).toHaveBeenCalledWith(expect.objectContaining(config));
+});
+
+describe("where methods", () => {
+  const url = "$URL";
+  const payload = { one: "first", two: "second" };
+
+  beforeEach(() => {
+    mockAxiosInstance.request.mockResolvedValue({});
+  });
+
+  const methods = ["post", "patch", "delete"];
+
+  methods.forEach((method) => {
+    describe(method.toUpperCase(), () => {
+      it(`sends ${method.toUpperCase()} request to correct URL`, () => {
+        const instance = new Requester();
+        instance[method](url, payload);
+
+        expect(mockAxiosInstance.request).toHaveBeenCalledWith(
+          expect.objectContaining({
+            method,
+            url,
+          })
+        );
+      });
+
+      it("sends payload as data", () => {
+        const instance = new Requester();
+        instance[method](url, payload);
+
+        expect(mockAxiosInstance.request).toHaveBeenCalledWith(
+          expect.objectContaining({
+            data: payload,
+          })
+        );
+      });
+
+      it("sends transformed payload as data (via `transformRequestData`)", () => {
+        const transformRequestData = (data) => ({
+          wrapper: data,
+          another: true,
+        });
+
+        const instance = new Requester({ transformRequestData });
+        instance[method](url, payload);
+
+        expect(mockAxiosInstance.request).toHaveBeenCalledWith(
+          expect.objectContaining({
+            data: transformRequestData(payload),
+          })
+        );
+      });
+    });
+  });
+
+  describe("GET", () => {
+    it("sends GET request to correct URL", () => {
+      const instance = new Requester();
+      instance.get(url, payload);
+
+      expect(mockAxiosInstance.request).toHaveBeenCalledWith(
+        expect.objectContaining({
+          method: "get",
+          url,
+        })
+      );
+    });
+
+    it("sends payload as params", () => {
+      const instance = new Requester();
+      instance.get(url, payload);
+
+      expect(mockAxiosInstance.request).toHaveBeenCalledWith(
+        expect.objectContaining({
+          params: payload,
+        })
+      );
+    });
+  });
+});
+
+it("injects headers at the moment of making a request (via `injectHeaders`)", () => {
+  const injectHeaders = jest.fn();
+  const instance = new Requester({ injectHeaders });
+  mockAxiosInstance.request.mockResolvedValue({});
+
+  expect(injectHeaders).not.toHaveBeenCalled();
+  instance.get();
+  expect(injectHeaders).toHaveBeenCalled();
+});
+
+describe("when request succeeded", () => {
+  const requestResult = {
+    data: {
+      one: 1,
+    },
+  };
+
+  beforeEach(() => {
+    mockAxiosInstance.request.mockResolvedValue(requestResult);
+  });
+
+  it("returns data by default", async () => {
+    const instance = new Requester();
+
+    await expect(instance.get()).resolves.toEqual(requestResult.data);
+  });
+
+  it("returns transformed data (via `transformResponse`)", async () => {
+    const transformResponse = (response) => response.data.one;
+    const instance = new Requester({ transformResponse });
+
+    await expect(instance.get()).resolves.toEqual(
+      transformResponse(requestResult)
+    );
+  });
+
+  it("returns data when custom transformResponse failed", async () => {
+    const transformResponse = () => {
+      throw new Error("Exception from transformResponse");
+    };
+    const instance = new Requester({ transformResponse });
+
+    await expect(instance.get()).resolves.toEqual(requestResult.data);
+  });
+});
+
+describe("when request failed", () => {
+  const requestResult = {
+    data: {
+      one: 1,
+    },
+  };
+
+  beforeEach(() => {
+    mockAxiosInstance.request.mockRejectedValue({ response: requestResult });
+  });
+
+  it("returns data by default", async () => {
+    const instance = new Requester();
+
+    await expect(instance.get()).rejects.toEqual(requestResult.data);
+  });
+});
